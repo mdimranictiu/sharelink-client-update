b263bd8b096380203a2bc584643c235fcf91974b
mdimranictiu <imranictiu11@gmail.com>
2025-03-02 11:15:40 +0600

    navbar and footer done

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import NavBar from '../sharedComponents/NavBar';
+import Footer from '../sharedComponents/Footer';
 
 const Root = () => {
     return (
         <>
+        <div>
         <NavBar></NavBar>
+        <div className='w-4/5 max-md:w-[90%] max-sm:w-[90%] my-5 mx-auto'>
         <Outlet></Outlet>
+        </div>
+        <Footer></Footer>
+        </div>
         </>
     );
 };
---
+++ src/sharedComponents/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-[#2A8AA4] text-white mt-[300px] text-center py-4">
+      <p>&copy; {new Date().getFullYear()} LinkShare. All rights reserved.</p>
+    </div>
+  );
+};
+
+export default Footer;
